0003dbbbb59b5c54a2ebc06a57bb217aa03b7fad
Abdellaho <bojaneabdellaho@gmail.com>
2018-11-18 17:09:34 +0100

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Application Test of Abdellah1';
+  title = 'Application Test of Abdellaho';
 }
